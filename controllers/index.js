9f0ff48eca0f9bd0d4159eaaaec38a246d5dd3f4
josha99528 <joshandrews@Josh-MacBook-Air.local>
2015-03-14 11:51:19 -0700

    semicolons...

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,9 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 var options = { root: './public/' };
 
 router.use('/sample-controller', require('./sample-controller'));
-// router.use('/users', require('./users'))
 
 router.get('/', function(req, res) {
     res.sendFile('index.html', options);
--- models/sample-model.js
+++ models/sample-model.js
@@ -1,8 +1,8 @@
 exports.create = function(callback) {
     // callback with result
-}
+};
 
 exports.get = function(callback) {
     var err = {};
     callback(err, 'Hello, world!');
-}
+};
